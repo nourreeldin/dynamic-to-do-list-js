76e75a99bbab958c7d4ee4397b6f32ec14dbff6e
Noureldin <noureeldinhossamm@gmail.com>
2025-04-03 03:14:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function () {
             newTask.textContent = taskText;
             const btn = document.createElement("button"); 
             btn.textContent = "Remove";
-            btn.className = 'remove-btn';
+            btn.classList.add('remove-btn');
             btn.onclick = () => {
                 newTask.remove();
             };
