791b56f6a5922bbda21d65bc686ed22a421dba5d
Noureldin <noureeldinhossamm@gmail.com>
2025-04-03 03:06:14 +0200

    Add script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const addButton = document.getElementById('add-task-btn');
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+
+    const addTask = () => {
+        const taskText = taskInput.value.trim();
+        if (taskText === "") {
+            alert("Invalid Input!");
+        } else {
+            const newTask = document.createElement("li");
+            newTask.textContent = taskText;
+            const btn = document.createElement("button"); 
+            btn.textContent = "Remove";
+            btn.className = 'remove-btn';
+            btn.onclick = () => {
+                newTask.remove();
+            };
+            newTask.appendChild(btn);
+            taskList.appendChild(newTask);
+            taskInput.value = "";
+        }
+    };
+    addButton.addEventListener("click", addTask);
+    taskInput.addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
+            addTask();
+        }
+    });
+});
